59cb495b53153a8305bedf672a219854b6b9129c
crazyquark <sam.bitcoin@gmail.com>
2015-12-08 19:51:02 +0200

    Does this fucking work?

---
+++ provisioners/src/main/java/provisioner/ProvisionerEthereum.java
@@ -0,0 +1,10 @@
+package provisioner;
+
+public class ProvisionerEthereum implements ProvisionerService {
+
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ provisioners/src/main/java/provisioner/ProvisionerEthereumServlet.java
@@ -0,0 +1,31 @@
+package provisioner;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.googlecode.jsonrpc4j.JsonRpcServer;
+
+public class ProvisionerEthereumServlet extends HttpServlet {
+	/**
+	 * Is this serializable or dancer?
+	 */
+	private static final long serialVersionUID = -7702784587269520126L;
+	private JsonRpcServer jsonRpcServer;
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    	try {
+			this.jsonRpcServer.handle(req, resp);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+
+    public void init(ServletConfig config) {
+        this.jsonRpcServer = new JsonRpcServer(new ProvisionerEthereumServlet(), ProvisionerService.class);
+    }
+}
