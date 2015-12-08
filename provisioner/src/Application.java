e332760c4edbcfe9e6f7bd11df5e1bf3df949b35
Andrei Grigoriu <mudpedal@vertexarmy.org>
2015-12-08 16:10:07 +0200

    Java main class and update gitignore for build

---
+++ provisioner/src/Application.java
@@ -0,0 +1,8 @@
+/**
+ * Created by andreicg on 12/8/15.
+ */
+public class Application {
+    public static void main (String [] args) {
+        System.out.println("Starting provisioner ...");
+    }
+}
