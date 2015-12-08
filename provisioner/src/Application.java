3c8ca4ccbbe7f5a11f18f78625fdbab39e016f11
crazyquark <sam.bitcoin@gmail.com>
2015-12-08 16:42:56 +0200

    Java main class and update gitignore for build

--- provisioner/src/Application.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by andreicg on 12/8/15.
- */
-public class Application {
-    public static void main (String [] args) {
-        System.out.println("Starting provisioner ...");
-    }
-}
