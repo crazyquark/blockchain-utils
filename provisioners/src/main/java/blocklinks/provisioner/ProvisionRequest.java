781e9e161c2467c64b08e3d2bfdda1982b9320ac
crazyquark <sam.bitcoin@gmail.com>
2015-12-09 13:33:16 +0200

    Provision multiple accounts at once

---
+++ provisioners/src/main/java/blocklinks/provisioner/ProvisionRequest.java
@@ -0,0 +1,63 @@
+package blocklinks.provisioner;
+
+public class ProvisionRequest {
+	private String account;
+	private float amount;
+	private String currency;
+	
+	/**
+	 * Create a provision request
+	 * 
+	 * @param account account to send currency to
+	 * @param amount amount to send
+	 */
+	public ProvisionRequest(String account, float amount, String currency) {
+		this.setAccount(account);
+		this.setAmount(amount);
+		this.currency = currency;
+	}
+
+	/**
+	 * @return the account
+	 */
+	public String getAccount() {
+		return account;
+	}
+
+	/**
+	 * @param account the account to set
+	 */
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	/**
+	 * @return the amount
+	 */
+	public float getAmount() {
+		return amount;
+	}
+
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+	/**
+	 * @return the currency
+	 */
+	public String getCurrency() {
+		return currency;
+	}
+
+	/**
+	 * @param currency the currency to set
+	 */
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	
+	
+}
--- provisioners/src/main/java/blocklinks/provisioner/ProvisioningService.java
+++ provisioners/src/main/java/blocklinks/provisioner/ProvisioningService.java
@@ -1,5 +1,17 @@
 package blocklinks.provisioner;
 
 public interface ProvisioningService {
-	int getCount();
+	
+	/**
+	 * This returns the account used for provisioning accounts
+	 * @return an account address(Ethereum, Ripple, BTC etc...)
+	 */
+	public String getRootAccount();
+	
+	/**
+	 * Provisions account with the given amount of currency
+	 * @param requests requests to execute
+	 * @return
+	 */
+	public int provision(ProvisionRequest[] requests);
 }
